7d5feaf8421cf651077bc04966ddfe7ef2345409
Jude Nelson <judecn@gmail.com>
2017-06-19 11:00:26 -0400

    implement storage driver selection based on replication strategy

--- src/datastore.js
+++ src/datastore.js
@@ -53,6 +53,11 @@ const EREMOTEIO = 121;
 
 const LOCAL_STORAGE_ID = "blockstack";
 const SUPPORTED_STORAGE_CLASSES = ["read_public", "write_public", "read_private", "write_private", "read_local", "write_local"];
+const REPLICATION_STRATEGY_CLASSES = {
+   'local': new Set(['read_local', 'write_local']),
+   'public': new Set(['read_public', 'write_public']),
+   'private': new Set(['read_private', 'write_private']),
+};
 
 /*
  * Helper method to validate a JSON response
@@ -565,6 +570,111 @@ function getUserData() {
 
 
 /*
+ * Fulfill a replication strategy using the drivers available to us.
+ *
+ * replication_strategy (object): a dict that maps strategies (i.e. 'local', 'public', 'private') to integer counts
+ * classes (object): this is session.storage.classes (i.e. the driver classification; maps a driver name to its list of classes)
+ *
+ * Returns the list of drivers to use.
+ * Throws on error.
+ */
+function selectDrivers(replication_strategy, classes) {
+
+   // select defaults from classification and replication strategy
+   let driver_sets = [];            // driver_sets[i] is the set of drivers that support SUPPORTED_STORAGE_CLASSES[i]
+   let driver_classes = {};         // map driver name to set of classes
+   let all_drivers = new Set([]);   // set of all drivers available to us
+   let available_drivers = [];      // drivers available to us
+   let selected_drivers = [];       // drivers compatible with our replication strategy (return value)
+   let have_drivers = false;        // whether or not we selected drivers that fulfill our replication strategy
+
+   for (let i = 0; i < SUPPORTED_STORAGE_CLASSES.length; i++) {
+      let driver_set = new Set(classes[SUPPORTED_STORAGE_CLASSES[i]]);
+      driver_sets.push(driver_set);
+
+      for(let d of driver_set) {
+          all_drivers.add(d);
+      }
+
+      for( let d of driver_set ) {
+         console.log(`Driver ${d} implementes ${SUPPORTED_STORAGE_CLASSES[i]}`);
+         if (driver_classes[d]) {
+            driver_classes[d].push(SUPPORTED_STORAGE_CLASSES[i]);
+         }
+         else {
+            driver_classes[d] = [SUPPORTED_STORAGE_CLASSES[i]];
+         }
+      }
+   }
+
+   let concern_fulfillment = {};
+
+   for (let d of all_drivers) {
+      let classes = driver_classes[d];
+     
+      // a driver fits the replication strategy if all of its
+      // classes matches at least one concern (i.e. 'local', 'public')
+      for (let concern of Object.keys(replication_strategy)) {
+
+          let matches = false;
+          for (let dclass of classes) {
+             if (REPLICATION_STRATEGY_CLASSES[concern].has(dclass)) {
+                matches = true;
+                break;
+             }
+          }
+
+          if (matches) {
+             console.log(`Driver ${d} fulfills replication concern ${concern}`);
+
+             if (concern_fulfillment[concern]) {
+                concern_fulfillment[concern] += 1;
+             }
+             else {
+                concern_fulfillment[concern] = 1;
+             }
+
+             if (concern_fulfillment[concern] <= replication_strategy[concern]) {
+                console.log(`Select driver ${d}`);
+                selected_drivers.push(d);
+             }
+          }
+
+          // strategy fulfilled?
+          let fulfilled = true;
+          for (let concern of Object.keys(replication_strategy)) {
+             let count = 0;
+             if (concern_fulfillment[concern]) {
+                count = concern_fulfillment[concern];
+             }
+
+             if (count < replication_strategy[concern]) {
+                fulfilled = false;
+                break;
+             }
+          }
+
+          if (fulfilled) {
+             have_drivers = true;
+             break;
+          }
+      }
+
+      if (have_drivers) {
+         break;
+      }
+   }
+
+   if (!have_drivers) {
+      throw new Error("Unsatisfiable replication strategy");
+   }
+
+   return selected_drivers;
+}
+
+
+
+/*
  * Connect to or create a datastore.
  * Asynchronous, returns a Promise
  *
@@ -572,7 +682,7 @@ function getUserData() {
  * Returns a Promise that yields a datastore connection, or an error object with .error defined.
  *
  */
-export function datastoreConnectOrCreate(storage_classes=["read_public", "write_private"], replicas=1, sessionToken=null, appPrivateKey=null) {
+export function datastoreConnectOrCreate(replication_strategy={'public': 1, 'local': 1}, sessionToken=null, appPrivateKey=null) {
    
    if(!sessionToken) {
       const userData = getUserData();
@@ -589,17 +699,17 @@ export function datastoreConnectOrCreate(storage_classes=["read_public", "write_
    }
 
    // sanity check 
-   for (let i = 0; i < storage_classes.length; i++) {
+   for (let strategy of Object.keys(replication_strategy)) {
       let supported = false;
-      for (let j = 0; j < SUPPORTED_STORAGE_CLASSES.length; j++ ) {
-         if (storage_classes[i] === SUPPORTED_STORAGE_CLASSES[j]) {
+      for (let supported_strategy of Object.keys(REPLICATION_STRATEGY_CLASSES)) {
+         if (supported_strategy === strategy) {
             supported = true;
             break;
          }
       }
 
       if (!supported) {
-         throw new Error(`Unsupported storage class ${storage_classes[i]}`);
+         throw new Error(`Unsupported replication strategy ${strategy}`);
       }
    }
 
@@ -616,41 +726,9 @@ export function datastoreConnectOrCreate(storage_classes=["read_public", "write_
    }
    else {
 
-      // select defaults from classification
-      let driver_sets = [];
-      let all_drivers = new Set([]);
-      for (let i = 0; i < storage_classes.length; i++) {
-         let driver_set = new Set(session.storage.classes[storage_classes[i]]);
-         driver_sets.push(driver_set);
-
-         for(let d of driver_set) {
-             all_drivers.add(d);
-         }
-      }
-
-      let available_drivers = [];
-      for (let d of all_drivers) {
-
-         let available = true;
-         for (let i = 0; i < driver_sets.length; i++) {
-            if (!driver_sets[i].has(d)) {
-               available = false;
-               break;
-            }
-         }
-
-         if (available) {
-            available_drivers.push(d);
-         }
-
-         if (available_drivers.length > replicas) {
-            break;
-         }
-      }
-
-      drivers = available_drivers;
+      // select defaults given the replication strategy
+      drivers = selectDrivers(replication_strategy, session.storage.classes);
    }
-
 
    const hostport = session.api_endpoint.split('://').reverse()[0];
    const appPublicKeys = session.app_public_keys;
