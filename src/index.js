fb5b8759cf5c15e153df5e6dcd608305d5656c75
Jude Nelson <judecn@gmail.com>
2017-08-31 11:52:40 -0400

    clean up exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
 'use strict'
 
+export {
+   getFile,
+   putFile,
+   deleteFile,
+   listFiles,
+   getFileURLs
+} from './api'
+
 export * from './errors';
-export * from './util';
-export * from './datastore';
-export * from './inode';
-export * from './schemas';
+
+export {
+   datastoreGetId,
+   datastoreMountOrCreate,
+   datastoreMount,
+   datastoreDelete,
+} from './datastore';
+
+export {
+   getUserData,
+   setUserData,
+} from './metadata';
+
+export {
+   decodePrivateKey,
+} from './util';
